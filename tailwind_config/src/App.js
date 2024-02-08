1348f9ffeecba93977b88a084118d3f70dbce0c0
Poonam Gahatraj <poonamgtrz@gmail.com>
2024-02-08 12:22:04 +0545

    tailwind_config

---
+++ tailwind_config/src/App.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+import Homepage from './pages/homepage';
+
+
+export default  function Hello (){
+  return(
+    <Homepage/>
+  )
+}
---
+++ tailwind_config/src/pages/homepage.js
@@ -0,0 +1,5 @@
+export default function Homepage(){
+    return(
+        <h1 className="text-red-400 text-9xl border border-indigo-600">Hello world</h1>
+    )
+}
